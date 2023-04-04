44084f5b585dace0d9d6d806ba19847b16c8dc80
tomduese <thomas.lettau@live.de>
2023-04-04 23:38:56 +0200

    add adjusterd R^2

--- methods/dsfunctions.py
+++ methods/dsfunctions.py
@@ -1,3 +1,8 @@
 def rename_columns(df):
     df.columns = df.columns.str.lower().str.replace(" ", "_")
     return df
+
+# Define function for calculating adjusted r-squared
+def adjusted_r_squared(r_squared, X):
+    adjusted_r2 = 1 - ((1 - r_squared) * (len(X) - 1) / (len(X) - X.shape[1] - 1))
+    return adjusted_r2 
