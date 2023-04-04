1830f3cdf47b5565245a9dd2ca29802c4d765e9a
tomduese <thomas.lettau@live.de>
2023-04-04 13:47:39 +0200

    add gradient descent functions

--- methods/gradient_descent.py
+++ methods/gradient_descent.py
@@ -1,48 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def  cal_cost(b, X, y):
-    '''
+
+def cal_cost(b, X, y):
+    """
     Calculates the cost for given X and y. The following shows and example of a single dimensional X
-    b = Vector of parameters 
+    b = Vector of parameters
     X     = Row of X's np.zeros((2,j))
     y     = Actual y's np.zeros((2,1))
-    
+
     where:
         j is the # of features
-    '''
-    
+    """
+
     n = len(y)
-    
+
     predictions = X.dot(b)
-    cost = 1/(2*n) * np.sum(np.square(predictions-y))
+    cost = 1 / (2 * n) * np.sum(np.square(predictions - y))
     return cost
 
+
 def gradient_descent(X, y, b, learning_rate=0.01, iterations=100):
-    '''
+    """
     X          = Matrix of X with added bias units
     y          = Vector of y
     b          = Vector of parameters np.random.randn(j,1)
-    learning_rate 
+    learning_rate
     iterations = # of iterations
-    
+
     Returns the final b vector and array of cost history over # of iterations
-    '''
+    """
     n = len(y)
     cost_history = np.zeros(iterations)
-    b_history = np.zeros((iterations,2))
-    
+    b_history = np.zeros((iterations, 2))
+
     for it in range(iterations):
-        
-        prediction = np.dot(X,b)
-        
-        b = b - learning_rate * (1/n) * (X.T.dot((prediction - y)))
-        b_history[it,:] = b.T
-        cost_history[it]  = cal_cost(b, X, y)
-        
+        prediction = np.dot(X, b)
+
+        b = b - learning_rate * (1 / n) * (X.T.dot((prediction - y)))
+        b_history[it, :] = b.T
+        cost_history[it] = cal_cost(b, X, y)
+
     return b, cost_history, b_history
 
-def plot_GD(X, y, n_iter,lr, ax, ax1=None):
+
+def plot_GD(X, y, n_iter, lr, ax, ax1=None):
     """
     n_iter = # of iterations
     lr = Learning Rate
@@ -51,9 +53,9 @@ def plot_GD(X, y, n_iter,lr, ax, ax1=None):
 
     """
 
-    X_b = np.c_[np.ones((len(X),1)),X]
-    _ = ax.plot(X, y, 'b.')
-    b = np.random.randn(2,1)
+    X_b = np.c_[np.ones((len(X), 1)), X]
+    _ = ax.plot(X, y, "b.")
+    b = np.random.randn(2, 1)
 
     tr = 0.1
     cost_history = np.zeros(n_iter)
@@ -64,62 +66,65 @@ def plot_GD(X, y, n_iter,lr, ax, ax1=None):
 
         cost_history[i] = h[0]
 
-        if ((i % 25 == 0) ):
-            _ = ax.plot(X, pred, 'r-', alpha=tr)
+        if i % 25 == 0:
+            _ = ax.plot(X, pred, "r-", alpha=tr)
             if tr < 0.8:
-                tr = tr+0.2
-    if not ax1== None:
-        _ = ax1.plot(range(n_iter), cost_history, 'b.')  
+                tr = tr + 0.2
+    if not ax1 == None:
+        _ = ax1.plot(range(n_iter), cost_history, "b.")
+
 
 def plot_GD_with_iter(X, y):
     """
     X = 2 * np.random.rand(100,1)
     y = 4 + 3 * X + np.random.randn(100,1)
+
+    to plot the figure simply run:
     fig = gd.plot_GD_with_iter(X,y)
     plt.show()
     """
-    fig = plt.figure(figsize=(10,8))
+    fig = plt.figure(figsize=(10, 8))
     fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
-    it_lr = [(2000,0.001), (500,0.01), (200,0.05), (100,0.1)]
+    it_lr = [(2000, 0.001), (500, 0.01), (200, 0.05), (100, 0.1)]
     count = 0
     for n_iter, lr in it_lr:
         count += 1
-        
+
         ax = fig.add_subplot(4, 2, count)
         count += 1
-    
+
         ax1 = fig.add_subplot(4, 2, count)
-        
+
         ax.set_title("lr:{}".format(lr))
         ax1.set_title("Iterations:{}".format(n_iter))
         plot_GD(X, y, n_iter, lr, ax, ax1)
     return fig
 
+
 def stochastic_gradient_descent(X, y, b, learning_rate=0.01, iterations=10):
-    '''
+    """
     X    = Matrix of X with added bias units
     y    = Vector of y
     b    = Vector of parameters np.random.randn(j,1)
-    learning_rate 
+    learning_rate
     iterations = # of iterations
-    
+
     Returns the final b vector and array of cost history over # of iterations
-    '''
+    """
     n = len(y)
     cost_history = np.zeros(iterations)
-    
-    
+
     for it in range(iterations):
         cost = 0.0
         for i in range(n):
             rand_ind = np.random.randint(0, n)
-            X_i = X[rand_ind,:].reshape(1, X.shape[1])
+            X_i = X[rand_ind, :].reshape(1, X.shape[1])
             y_i = y[rand_ind].reshape(1, 1)
             prediction = np.dot(X_i, b)
 
-            b = b - (1/1) * learning_rate * (X_i.T.dot((prediction - y_i)))
+            b = b - (1 / 1) * learning_rate * (X_i.T.dot((prediction - y_i)))
             cost += cal_cost(b, X_i, y_i)
-        cost_history[it]  = cost/n
-        
+        cost_history[it] = cost / n
+
     return b, cost_history
--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 import methods.dsplotlib as dsplot
 import methods.dsfunctions as dsf
-import methods.dsgradient_decent as gd
+import methods.gradient_descent as gd
+import methods.plotlyplotlib as pqq
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-# df = pd.read_csv("data/carseats.csv")
-# df = dsf.rename_columns(df)
+df = pd.read_csv("data/carseats.csv")
+df = dsf.rename_columns(df)
 # corr = df[["sales", "price", "shelveloc", "advertising", "age", "income", "us"]].corr()
 # fig, ax = dsplot.corrplot(corr)
 # plt.show()
 
 
-
+qqplots = pqq.make_qq_plots(df[["sales", "price", "advertising", "age", "income"]])
+qqplots.show()
+#
