01e331ea56b5f5794b06466299e93569012d4465
tomduese <thomas.lettau@live.de>
2023-04-05 14:40:15 +0200

    add plot_roc to dsmathplotlib

--- methods/dsmatplotlib.py
+++ methods/dsmatplotlib.py
@@ -1,3 +1,6 @@
+from sklearn.metrics import roc_curve
+
+
 # Create a line plot with Matplotlib and the given parameters: x, y, xlabel, ylabel, title
 def line_plot(x, y, xlabel="X", ylabel="Y", title="Line Plot"):
     plt.plot(x, y)
@@ -33,4 +36,15 @@ def bar_plot(
     plt.ylabel(ylabel)
     plt.title(title)
     plt.grid(axis="y")
+    plt.show()
+
+
+def plot_roc(actual_values, model_values):
+    fpr, tpr, thresholds_RF = roc_curve(actual_values, model_values)
+    plt.plot(fpr, tpr, "r-", label="RF")
+    plt.plot([0, 1], [0, 1], "k-", label="random")
+    plt.plot([0, 0, 1, 1], [0, 1, 1, 1], "g-", label="perfect")
+    plt.legend()
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
     plt.show()
