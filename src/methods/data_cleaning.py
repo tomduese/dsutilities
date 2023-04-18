ba187001ad620e3c6c3a51ab280316c5ee446175
tomduese <thomas.lettau@live.de>
2023-04-18 11:54:29 +0200

    add missing values function

--- src/methods/data_cleaning.py
+++ src/methods/data_cleaning.py
@@ -27,6 +27,24 @@ def clean_columnnames(df):
     return df
 
 
+def check_for_missing_values(df):
+    """
+    This function takes a pandas DataFrame as input and returns a DataFrame containing the number and percentage
+    of missing values in each column of the input DataFrame.
+
+    Parameters:
+        df (pandas.DataFrame): A pandas DataFrame.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the number and percentage of missing values in each column of
+        the input DataFrame.
+    """
+    missing = pd.DataFrame(df.isnull().sum(), columns=["Amount"])
+    missing["Percentage"] = round((missing["Amount"] / df.shape[0]) * 100, 2)
+    missing[missing["Amount"] != 0]
+    return missing
+
+
 def load_carseats():
     df = pd.read_csv("dsutilities/data/carseats.csv")
     df = clean_columnnames(df)
